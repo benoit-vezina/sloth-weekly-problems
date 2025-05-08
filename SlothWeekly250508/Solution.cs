410d9e52c2eeef81e096c964059989801d54433d
Benoit Vézina <benoit.vezina14@outlook.com>
2025-05-08 10:07:00 -0400

    Project setup and IsShuffledWell problem

---
+++ SlothWeekly250508/Solution.cs
@@ -0,0 +1,31 @@
+// Given an array of 10 numbers, return whether or not the array is shuffled enough.
+
+// In this case, if 3 or more numbers appear consecutively (ascending or descending), return false.
+
+namespace SlothWeekly250508;
+
+static class Solution
+{
+    // Simply increment a counter every time the difference with the last element is 
+    // 1 or -1. If we wanted to find the the longuest not-shuffled sequence, we would
+    // implement the Sliding Window technique.
+    // We assume that the numbers parameter is always an array of numbers from 1 to 10
+    // arranged in a random order
+    public static bool IsShuffledWell(int[] numbers)
+    {
+        int lastNumber = numbers[0];
+        int consecutiveNumberCount = 1;
+
+        for (int i = 1; i < numbers.Length; i++)
+        {
+            if (Math.Abs(numbers[i] - lastNumber) == 1)
+            {
+                if (++consecutiveNumberCount == 3) return false;
+            }
+            else consecutiveNumberCount = 1;
+            lastNumber = numbers[i];
+        }
+
+        return true;
+    }
+}
---
+++ SlothWeekly250508/SolutionTest.cs
@@ -0,0 +1,15 @@
+﻿namespace SlothWeekly250508;
+
+public class SolutionTest
+{
+    [Theory]
+    [InlineData(new int[]{1, 2, 3, 5, 8, 6, 9, 10, 7, 4}, false)]
+    [InlineData(new int[]{3, 5, 1, 9, 8, 7, 6, 4, 2, 10}, false)]
+    [InlineData(new int[]{1, 5, 3, 8, 10, 2, 7, 6, 4, 9}, true)]
+    [InlineData(new int[]{1, 3, 5, 7, 9, 2, 4, 6, 8, 10}, true)]
+    [InlineData(new int[]{10, 8, 9, 7, 5, 4, 6, 3, 2, 1}, false)]
+    public void Solution_IsShuffledWell_ReturnsExpectedOutput(int[] numbers, bool expectedOutput)
+    {
+        Assert.Equal(Solution.IsShuffledWell(numbers), expectedOutput);
+    }
+}
